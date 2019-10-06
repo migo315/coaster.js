8fac47c5475f93a61bf7182ad32d9e3f210d74b5
Philip McKenna <philip.mckenna@bazaarvoice.com>
2019-10-06 13:12:25 +0100

    test: build query tests

---
+++ tests/build-query.test.js
@@ -0,0 +1,117 @@
+const client = require('../src/client');
+
+const stubbedConfig = require('./test-data/stubbed-config');
+
+describe('Build Query', () => {
+  describe('when a valid config is passed into buildQuery', () => {
+    const expectedQuery =
+      '?page=98&itemsPerPage=99&search=soup&language=Gaelic&sort=Ascending&filter[]=regulation(182%2C26)&facet[]=facets&facet[]=exist&filter[]=filters&filter[]=exist';
+
+    let result;
+
+    beforeEach(() => {
+      result = client.buildQuery(stubbedConfig);
+    });
+
+    it('returns the expected query', () => {
+      expect(result).toEqual(expectedQuery);
+    });
+  });
+
+  describe('when an empty config is passed into the buildQuery', () => {
+    let result;
+
+    beforeEach(() => {
+      result = client.buildQuery({});
+    });
+
+    it('returns only the default language', () => {
+      expect(result).toEqual('?language=en');
+    });
+  });
+
+  describe('when a config is passed with invalid regulation fields', () => {
+    const expectedQueryNoRegulationValues =
+      '?page=98&itemsPerPage=99&search=soup&language=Gaelic&sort=Ascending&facet[]=facets&facet[]=exist&filter[]=filters&filter[]=exist';
+
+    let result;
+
+    const invalidRegulationFields = {
+      ...stubbedConfig,
+      regulation: {
+        bad: 'field',
+      },
+    };
+
+    beforeEach(() => {
+      result = client.buildQuery(invalidRegulationFields);
+    });
+
+    it('returns no regulation values', () => {
+      expect(result).toEqual(expectedQueryNoRegulationValues);
+    });
+  });
+
+  describe('when a config is passed with a non-numeric regulation size', () => {
+    const expectedQueryRegulationSizeNull =
+      '?page=98&itemsPerPage=99&search=soup&language=Gaelic&sort=Ascending&filter[]=regulation(null%2C26)&facet[]=facets&facet[]=exist&filter[]=filters&filter[]=exist';
+
+    let result;
+
+    const invalidRegulationFields = {
+      ...stubbedConfig,
+      regulation: {
+        size: '6ft',
+        age: 26
+      },
+    };
+
+    beforeEach(() => {
+      result = client.buildQuery(invalidRegulationFields);
+    });
+
+    it('returns regulation size as null', () => {
+      expect(result).toEqual(expectedQueryRegulationSizeNull);
+    });
+  });
+
+  describe('when a config is passed with a non-numeric regulation age', () => {
+    const expectedQueryRegulationAgeNull =
+    '?page=98&itemsPerPage=99&search=soup&language=Gaelic&sort=Ascending&filter[]=regulation(182%2Cnull)&facet[]=facets&facet[]=exist&filter[]=filters&filter[]=exist';
+
+    let result;
+
+    const invalidRegulationFields = {
+      ...stubbedConfig,
+      regulation: {
+        size: 182,
+        age: 'Twenty Six',
+      },
+    };
+
+    beforeEach(() => {
+      result = client.buildQuery(invalidRegulationFields);
+    });
+
+    it('returns regulation size as null', () => {
+      expect(result).toEqual(expectedQueryRegulationAgeNull);
+    });
+  });
+
+  describe('when withAcl is true', () => {
+    const expectedQuery =
+      '?page=98&itemsPerPage=99&search=soup&language=Gaelic&sort=Ascending&filter[]=regulation(182%2C26)&facet[]=facets&facet[]=exist&filter[]=filters&filter[]=exist';
+
+    let result;
+
+
+    beforeEach(() => {
+      client.withAcl = true;
+      result = client.buildQuery(stubbedConfig);
+    });
+
+    it('returns the expected query', () => {
+      expect(result).toEqual(expectedQuery+'&acl=true');
+    });
+  });
+});
